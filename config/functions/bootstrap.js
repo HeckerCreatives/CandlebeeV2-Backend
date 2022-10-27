9d7fdae1057a6bf6d8f9c10bf8f1caaae4fc3c96
Emman <emmanagan1995@gmail.com>
2022-10-27 17:10:57 +0800

    test

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,5 @@
 const WebSocket  = require('ws')
+const axios = require('axios');
 
 'use strict';
 /**
@@ -11,20 +12,38 @@ const WebSocket  = require('ws')
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = () => {
+module.exports = async () => {
     const ws = new WebSocket('wss://fstream.binance.com/ws/btcusdt@kline_3m');
+    const countBetHistory = await strapi.api["current-round"].services["current-round"].count();
+
+    if(countBetHistory <= 0) {
+      const binanceData = await axios("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=3m" )
+      const { data } = binanceData;
+
+      const finalData = data[data.length - 1]
+
+      await strapi.api["bet-history"].services["bet-history"].create({
+        winning_color:
+        parseFloat(data[data.length - 1][4]) > parseFloat(finalData[4])
+          ? "red"
+          : "green",
+        open: parseFloat(finalData[1]),
+        close: parseFloat(finalData[4]),
+        high: parseFloat(finalData[2]),
+        low: parseFloat(finalData[3]),
+      });
+    }
 
     ws.on('message', async (data) => {
         if (data) {
           try {
            const bin = JSON.parse(data); 
+           if(countBetHistory > 0) {
             if (bin.k.x) {
                const betHistoryLatest = await strapi.query('bet-history').model.find({}).sort({_id: -1}).limit(1);
-               const winningColor = betHistoryLatest[0].close > parseFloat(bin.k.c)
-                    ? "red"
-                    : "green";
+               const winningColor = betHistoryLatest[0].close > parseFloat(bin.k.c)? "red" : "green";
 
-              await strapi.api["bet-history"].services["bet-history"].create({
+                await strapi.api["bet-history"].services["bet-history"].create({
                   winning_color: winningColor,
                   open: parseFloat(bin.k.o),
                   close: parseFloat(bin.k.c),
@@ -34,6 +53,7 @@ module.exports = () => {
               await strapi.api["current-round"].services["current-round"].payoutPlayers(winningColor);
               await strapi.query('current-round').model.deleteMany({});
             } 
+           }
           } catch (err) {
             console.log(err)
           }
