efc76422f42495cb9c4cd3861c233389e52966da
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2025-11-24 08:25:04 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.l63dw.mongodb.net'),
+        host: env('DATABASE_HOST', 'creativebraindevelopmen.itmjhkl.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'candlebeev2-db'),
-        username: env('DATABASE_USERNAME', 'emman24'),
-        password: env('DATABASE_PASSWORD', 'wewename1234'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
